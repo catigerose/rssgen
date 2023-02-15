4f936028f4a7dfac615d5154a85f6b629115e8fe
catigerose <67047102+catigerose@users.noreply.github.com>
2023-02-15 23:28:51 +0800

    Update feed_funcs.py

--- feed_funcs.py
+++ feed_funcs.py
@@ -15,7 +15,7 @@ import json
 # 根据操作系统指定工作目录，使代码在linux和windows都能运行。
 if system() == 'Linux':
     work_dir = "/usr/share/rssgen"
-    chromedriver_path = /usr/share/rssgen/chromedriver-linux/chromedriver  # chromedriver的路径
+    chromedriver_path = "/usr/share/rssgen/chromedriver-linux/chromedriver" # chromedriver的路径
 elif system() == 'Windows':
     work_dir = "."
     chromedriver_path = "./chromedriver-windows/chromedriver"  # chromedriver的路径
